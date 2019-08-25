61cedba42995e16bd483c842e92979c2d998c74a
annzk <903196504@qq.com>
2019-08-25 10:29:59 +0800

    add ok Http

--- src/main/java/com/majiang/community/Controller/indexController.java
+++ src/main/java/com/majiang/community/Controller/indexController.java
@@ -1,4 +1,12 @@
 package com.majiang.community.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class indexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
--- src/main/java/com/majiang/community/dto/GithubUser.java
+++ src/main/java/com/majiang/community/dto/GithubUser.java
@@ -1,4 +1,31 @@
 package com.majiang.community.dto;
 
 public class GithubUser {
+    private Long id;
+    private String bio;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
